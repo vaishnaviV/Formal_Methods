ed4533d6a89c1fa09e11df1419998d79455e1b45
Vaishnavi <vaishu.maverick@gmail.com>
2016-04-24 20:58:54 -0500

    Boolean Conversion

---
+++ untitled/bin/main.dart
@@ -0,0 +1,10 @@
+main(List<String> args) {
+ double a = 10.5;
+ double b = 2.3;
+ double c = a+b;
+  //while (1) there is no implicit conversion. so use while (1 == 1) for boolean
+    while (1 == 1)
+  {
+      print (c);
+  }
+}
