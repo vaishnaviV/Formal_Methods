b130b5ebea327bbb3ca0232a52f4a4a5a33194b7
Vaishnavi <vaishu.maverick@gmail.com>
2016-04-24 19:40:37 -0500

    Area

---
+++ Area/bin/main.dart
@@ -0,0 +1,40 @@
+void CircleArea()
+{
+      double ar, rad=5.0;
+      const double pi = 3.14;
+      ar  = pi * rad * rad;
+       print (' circle area : $ar');
+}
+
+void TriangleArea()
+{
+      double ar, base =7.0 , height=6.5;
+      ar = 0.5 * base * height;
+  print (' triangle area : $ar');
+}
+
+void RectangleArea()
+{idth=4.6;
+       ar = length * width;
+      print(' rectangle area : $ar');
+double ar, length=12.8, w
+}
+
+void SquareArea()
+{
+     double ar, side=9.0;
+     ar = side * side;
+     print (' square area : $ar');
+}
+
+
+
+main(List<String> args) {
+
+            CircleArea ();
+            TriangleArea();
+            RectangleArea();
+            SquareArea();
+
+
+}
