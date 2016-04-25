f974207c10f8f37380526b0187c2d7ba0f6bd490
Vaishnavi <vaishu.maverick@gmail.com>
2016-04-24 20:34:49 -0500

    Two Dimensional Array

---
+++ TwoDimArray/bin/main.dart
@@ -0,0 +1,13 @@
+  main(List<String> args) {
+    List <List> a = [
+      [5,6],
+      [9,10]
+    ];
+    List <List>  b = [
+      [1,2],
+      [5,6]
+    ];
+    print (a[0][0]);
+    print (b[1][1]);
+  }
+
