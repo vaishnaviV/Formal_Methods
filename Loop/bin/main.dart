56b3a998f6db551f0427afdf78546da6f98a5764
Vaishnavi <vaishu.maverick@gmail.com>
2016-04-24 19:57:21 -0500

    Infinte Loop

---
+++ Loop/bin/main.dart
@@ -0,0 +1,14 @@
+main(List<String> args) {
+ int age  = 10;
+ String name = 'Vaishnavi';
+ String gender = 'Female';
+
+         while (true)
+         {
+               print (name);
+               print (age);
+         }
+
+  print (gender);
+
+}
